7fba5dbcef567c505e9e30ddf96c51ccb12a5bcf
Andrew Glenn <andrew@andrewglenn.net>
2014-01-14 09:16:29 -0600

    removing 4x4 in set_layout

--- tmuxscript.sh
+++ tmuxscript.sh
@@ -1,45 +1,17 @@
 #!/bin/bash
-# 
-### Begin Software License.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# http://www.gnu.org/licenses/gpl-3.0.html
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# No Warranty or guarantee or suitability exists for the software.
-# Use at your own risk. The author is not responsible if your system breaks.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-### End Software License.
-# v: 0.2
-# r: 2013.08.14
-#
-# Things you can change. 
+
+## Things you can change. 
 #debug=true
-session_name="rax"
-max_win_panes="8"
+session_name="raxauto"
+max_win_panes="12"
 window_number="0"
-script_command="cd /tmp/tmux-test; touch"
+script_command="touch"
 
 ## Do not edit below this line.
 current_panes="1"
-declare -A layout_array=(['4x4']='4492,96x11,0,0[96x2,0,0{48x2,0,0,17,47x2,49,0,24},96x2,0,3{48x2,0,3,18,47x2,49,3,23},96x2,0,6{48x2,0,6,19,47x2,49,6,22},96x2,0,9{48x2,0,9,20,47x2,49,9,21}]' )
+#declare -A layout_array=(['4x4']='4492,96x11,0,0[96x2,0,0{48x2,0,0,17,47x2,49,0,24},96x2,0,3{48x2,0,3,18,47x2,49,3,23},96x2,0,6{48x2,0,6,19,47x2,49,6,22},96x2,0,9{48x2,0,9,20,47x2,49,9,21}]' )
 
 function window_check(){
-        if [ $debug ]; then
-           echo "Current Panes ($current_panes) is equal to Max Window Panes ($max_win_panes)! Time for a new window!"
-        fi
-        
         set_layout
         tmux set-window-option -t :$window_number synchronize-panes on
  
@@ -50,15 +22,11 @@ function window_check(){
 
         tmux select-window -t $session_name:$window_number
         launch_all_zig
-
-        if [ $debug ]; then        
-           echo "window_number => $window_number"
-           echo "current_panes => $current_panes"
-        fi
 }
 
 function launch_all_zig(){
     if [ "$1" == "first" ]; then
+        [ $i -eq 1 ]; i=0
         tmux send-keys -t $session_name:0.0 "$script_command ${raxinfo[$i]}" C-m
     else
         tmux send-keys "$script_command ${raxinfo[$i]}" C-m
@@ -69,10 +37,6 @@ function set_layout(){
 
     if [ "$1" == "tiled" ]; then
         layout_setup="tiled"
-    else
-        case $max_win_panes in 
-            8) layout_setup=${layout_array[4x4]} ;;
-        esac
     fi
     
     tmux select-layout -t $session_name:$window_number $layout_setup >/dev/null 2>&1
@@ -81,7 +45,7 @@ function set_layout(){
 function usage(){
     echo "Usage:"
     echo
-    echo "$0 [-l External List] [-s External Script] [-d Device List, space seperated]"
+    echo "$0 [-l External List] [-s External Script] [-t]"
     echo 
     echo "If no options are passed, $0 will look for 'list' within the current directory, and will execute as"
     echo "defined in the \$script_command variable at the top of the script."
@@ -89,6 +53,7 @@ function usage(){
     echo "Each value in 'list' (or what is provided with the -l option) will be the last argument for what is defined as \$script command"
     echo "Ex: ping 1.2.3.4, where ping is the \$script_command variable and '1.2.3.4' is a value within 'list'"
     echo
+    echo "-t is used for nested tmux windows"
     exit 1
 }
 
@@ -97,11 +62,12 @@ if [ $# -eq 0 ]; then
 fi
 
 OPTERR=0
-while getopts "s:l:d:"  OPTION; do 
+while getopts "tsn:l:"  OPTION; do 
     case "$OPTION" in
-        d) server_list="${OPTARG}";;
         s) external_script="${OPTARG}";;
         l) external_list="${OPTARG}"; total_panes=${#raxinfo[@]};;
+        n) custom_session_name="${OPTARG}";;
+        t) this_tmux_window=true;;
         ?) usage;; 
     esac
 done
@@ -110,12 +76,9 @@ done
 if [ -n "$external_list" ]; then 
     if [ -e "$external_list" ]; then  
         raxinfo=($(cat $external_list)); 
-    else 
-        echo "$external_list doesn't exist!"
-        exit 1
+    else
+        raxinfo=$external_list
     fi
-elif [ -n "$server_list" ]; then
-    raxinfo=($server_list)
 fi
 
 if [ -n "$external_script" ]; then
@@ -127,22 +90,25 @@ if [ -n "$external_script" ]; then
     fi
 fi
 
-tmux has-session -t $session_name >/dev/null 2>&1
+if test $custom_session_name; then
+    session_name=$custom_session_name
+fi
+
+
+if ! test $this_tmux_window; then 
+    tmux has-session -t $session_name >/dev/null 2>&1
+fi
 
 # If the tmux session named $session_name doesn't exist...
 if [ $? -eq 1 ]; then 
-    tmux new-session -s rax -d 
-fi
-
-if [ $debug ]; then 
-    echo "window_number => $window_number"
-    echo "current_panes => $current_panes"
+    tmux new-session -s $session_name -d 
 fi
 
 total_panes=${#raxinfo[@]}
-for i in $(seq 1 $((total_panes -1))); do
+for i in $(seq 0 $((total_panes -1))); do
     if [[ $current_panes -eq 1 ]]; then
         launch_all_zig "first"
+        first_post=true
     fi
     
 
@@ -153,26 +119,25 @@ for i in $(seq 1 $((total_panes -1))); do
     fi
 
 
-    if [ -z $new_window ]; then
+    if [ -z $new_window ] && [ ! $first_post ]; then
         tmux split-window -t $session_name:$window_number.0 -h -l 2
         launch_all_zig
     fi
 
     while [ $? -eq 1 ]; do
+        if $this_tmux_window; then 
+            break
+        fi
         window_number=$((window_number+1))
         tmux new-window -t $session_name:$window_number
         launch_all_zig
         current_panes=0
     done
    
-    if [ $debug ]; then 
-        echo -e "i => $i\t$window_number.$current_panes"
-    fi
-
     let  current_panes++
-    unset new_window
+    unset new_window first_post
 done
 
-set_layout
+set_layout "tiled"
 tmux set-window-option -t :$window_number  synchronize-panes on >/dev/null 2>&1
 tmux attach -d -t $session_name
